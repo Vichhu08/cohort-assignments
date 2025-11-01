134b3589dda9e8981b34fe4af858c1fe188c437f
vichhu08 <vichhuviswanathan@gmail.com>
2025-11-01 16:13:06 +0530

    initial commit

--- week-2/javascript.js
+++ week-2/javascript.js
@@ -5,3 +5,11 @@ function sum (n){
 }
 
 console.log(sum(10));
+
+const fs = require("fs");
+
+let contents = fs.readFileSync("./vichhu.txt", "utf-8"); // bytes,hex...
+console.log(contents);  // need to open terminal and type - node " javascript.js " to run. 
+
+let content2 = fs.readFileSync("./b.txt", "utf-8");
+console.log(content2);
