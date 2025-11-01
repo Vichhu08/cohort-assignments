09c648d82aecc7a1c3abbe87375607ba60ac5b13
vichhu08 <vichhuviswanathan@gmail.com>
2025-11-01 14:54:01 +0530

    initial commit

---
+++ week-1/javascript.js
@@ -0,0 +1,23 @@
+let names = "vichhu";
+console.log(names);
+const PI = 3.164;
+console.log(PI);
+
+function details (user) {
+    console.log(`hii ${user.nam}, your age is ${user.age} right?`) ;
+}
+
+let user = {
+    nam: "vichhu",
+    age: 20
+}
+
+details(user);
+
+function sum(a,b){
+    return parseInt(a) +parseInt(b);
+}
+
+let ans = sum(5,89);
+console.log(ans);
+
