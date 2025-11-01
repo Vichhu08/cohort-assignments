b147c3c9347354a24ec45077f1f8ebec7010654c
vichhu08 <vichhuviswanathan@gmail.com>
2025-11-01 19:03:59 +0530

    used Asynchornous method

--- week-2/javascript.js
+++ week-2/javascript.js
@@ -8,8 +8,22 @@ console.log(sum(10));
 
 const fs = require("fs");
 
+// synchonrously method
+
 let contents = fs.readFileSync("./vichhu.txt", "utf-8"); // bytes,hex...
 console.log(contents);  // need to open terminal and type - node " javascript.js " to run. 
 
 let content2 = fs.readFileSync("./b.txt", "utf-8");
 console.log(content2);
+
+
+// Asunchornously method
+
+function read (err,data){       
+    console.log(data);
+}
+fs.readFile("./vichhu.txt", "utf8", read);
+fs.readFile("./b.txt", "utf8", read);
+console.log("END Done!");
+
+
