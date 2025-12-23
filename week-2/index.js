f5c2c557a9be326f524f8f92618fd6205624b97f
vichhu08 <vichhuviswanathan@gmail.com>
2025-12-23 22:11:46 +0530

    initial commit

--- week-2/index.js
+++ week-2/index.js
@@ -9,9 +9,10 @@ setTimeout(timeout, 2500);
 console.log("welcome to loupe")
 
 let c = 0;
-for (let i = 0; i < 1000000000000; i++){
+for (let i = 0; i < 1000000000; i++){
     c += i;
 }
 
 console.log("done");
+
 
